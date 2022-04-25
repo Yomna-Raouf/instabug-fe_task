6946b25eadd611d69948d48641e2454ad94202d8
Yomna-Raouf <yomnaraouf99@gmail.com>
2022-04-25 19:03:46 +0200

    search component && filtration and highlight functionality && clear button functionality

--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -1,17 +1,19 @@
-angular
-  .module('appModule')
-  .controller('homeController', homePageController);
+angular.module('appModule').controller('homeController', homePageController);
 
 function homePageController(Employees) {
   const homePageVm = this;
   homePageVm.employees = [];
+  homePageVm.filterQuery = '';
 
   activate();
 
   function activate() {
-    Employees.getEmployees()
-      .then(({ data }) => {
-        homePageVm.employees = homePageVm.employees.concat(data.employees);
-      });
+    Employees.getEmployees().then(({ data }) => {
+      homePageVm.employees = homePageVm.employees.concat(data.employees);
+    });
   }
+
+  homePageVm.handleFilterEmployeesEvent = function (query) {
+    homePageVm.filterQuery = query;
+  };
 }
